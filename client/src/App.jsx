08b240bb2712a55869c5dd1a6c2010d09d105faf
William G (VinuSion) <revisionthegame@gmail.com>
2023-09-26 20:10:21 -0500

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import artihealLogo from './assets/artiheal-logo.svg'
-import './App.css'
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={artihealLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Artiheal + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <h1 className="text-3xl font-bold">Artiheal</h1>
+  );
 }
 
-export default App
+export default App;
