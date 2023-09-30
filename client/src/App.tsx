c1b5e11f0d4eddca3a437b459e6a53b75b36df91
William G (VinuSion) <revisionthegame@gmail.com>
2023-09-30 17:22:26 -0500

    button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div className="flex justify-center items-center flex-row mt-40">
       <h1 className="font-black lg:text-[80px] sm:text-[60px]text-[40px] lg:leading-[98px] mt-2 mr-5">Artiheal</h1>
-
-      
+      <Button>Entregar</Button>
     </div>
   );
 }
