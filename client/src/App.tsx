8f856fd81dc1d43efce845c5585bd52630a89e62
Jospeeth <japertuzd@gmail.com>
2023-09-30 16:47:05 -0500

    I have changed this shit, IS MY FUCKING PROBLEM

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="flex justify-center items-center flex-row mt-40">
       <h1 className="font-black lg:text-[80px] sm:text-[60px]text-[40px] lg:leading-[98px] mt-2 mr-5">Artiheal</h1>
+      <p>jose</p>
     </div>
   );
 }
