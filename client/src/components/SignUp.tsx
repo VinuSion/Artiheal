40043f5da29395a21f546b80c1d9c3bfb9490964
Jospeeth <japertuzd@gmail.com>
2023-10-03 21:04:43 -0500

    fix bugs again

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -9,7 +9,7 @@ const SignUp = () => {
         <div className="sign-up shadow-xl p-6 rounded-lg">
           <div className="icon flex items-center">
             <img
-              className="h-16, w-16"
+              className="h-14, w-14"
               src="../public/artiheal-logo.svg"
               alt=""
             />
@@ -43,43 +43,46 @@ const SignUp = () => {
           <div className="rest ">
             <div className="grid w-full max-w items-center gap-2 mt-5">
               <Label className="font-semibold" htmlFor="email">
-                Email Address
+                Correo electronico
               </Label>
               <Input
                 className="border rounded-lg p-2 transition duration-300 hover:shadow-md focus:shadow-md focus:ring-2 focus:ring-indigo-500"
                 type="email"
                 id="email"
-                placeholder="Email"
+                placeholder="Correo electronico"
               />
             </div>
             <div className="grid w-full max-w items-center gap-1.5 mt-5">
               <Label className="font-semibold" htmlFor="password">
-                Password
+                Contraseña
               </Label>
               <Input
                 className="border rounded-lg p-2 transition duration-300 hover:shadow-md focus:shadow-md focus:ring-2 focus:ring-indigo-500"
                 type="password"
                 id="password"
-                placeholder="password"
+                placeholder="Contraseña"
               />
             </div>
             <div className="grid w-full max-w items-center gap-1.5 mt-5">
               <Label className="font-semibold" htmlFor="password">
-                Confirm password
+                Confirmar Contraseña
               </Label>
               <Input
                 className="border rounded-lg p-2 transition duration-300 hover:shadow-md focus:shadow-md focus:ring-2 focus:ring-indigo-500"
                 type="password"
-                id="password"
-                placeholder="password"
+                id="confirm-password"
+                placeholder="Contraseña"
               />
             </div>
           </div>
-          <Button className="my-6" size={"superbig"}>
+          <Button
+            className=" my-4 bg-gradient-to-r from-purple-500 to-blue-600 hover:from-indigo-300 hover:to-purple-600 text-white font-bold py-3 px-6  shadow-lg transform transition-all duration-500 ease-in-out hover:brightness-110 hover:animate-pulse active:animate-bounce"
+            size={"superbig"}
+          >
             Continuar
           </Button>
           <span>
-            Ya estás en Artihel?{" "}
+            Ya estás en Artiheal?{" "}
             <Link className="text-indigo-500" to="/login">
               Iniciar sesión
             </Link>
