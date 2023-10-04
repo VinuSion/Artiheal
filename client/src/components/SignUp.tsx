c0b0cf7411f2cc8f14c4729f277bd0b5bdb5cfff
Michael Ferreira <micharrieta10@gmail.com>
2023-10-03 22:45:39 -0500

    Delete: Estas en artiheal

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -78,7 +78,6 @@ const SignUp = () => {
           </Button>
           <span>
             Ya estás en Artiheal? 
-            Ya estás en Artiheal? 
             <Link className="text-indigo-500 ml-2 hover:underline" to="/login">
               Iniciar sesión
             </Link>
