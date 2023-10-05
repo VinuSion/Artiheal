6d0f672fc209dc0d8e5b7ec7f48ed1754af81c88
William G (VinuSion) <revisionthegame@gmail.com>
2023-10-04 20:28:48 -0500

    merged login page

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -3,14 +3,13 @@ import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import { Checkbox } from "./ui/checkbox";
 import { Link } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 interface LoginProps {
   handleLogin: () => void; // Adjust the type of handleLogin as needed
 }
 
 const Login = ({ handleLogin }: LoginProps) => {
-
   const navigate = useNavigate();
 
   const handleLoginClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -20,7 +19,7 @@ const Login = ({ handleLogin }: LoginProps) => {
 
     // Call the handleLogin function to update authentication state
     handleLogin();
-    navigate('/dashboard');
+    navigate("/dashboard");
     // Redirect to the HomePage after successful login
     // Assuming that 'handleLogin' updates 'isAuth' state
   };
@@ -29,15 +28,17 @@ const Login = ({ handleLogin }: LoginProps) => {
     <div className="min-h-screen flex items-center justify-center bg-splash-image bg-cover bg-no-repeat bg-center">
       <main className="flex flex-col place-items-center">
         <div className="log-in shadow-2xl p-6 rounded-lg bg-background">
-
           <div className="icon flex items-center justify-center">
-            <img className="h-14, w-14 select-none" src="/artiheal-logo.svg" alt="logo" />
+            <img
+              className="h-14, w-14 select-none"
+              src="/artiheal-logo.svg"
+              alt="logo"
+            />
             <span className="font-bold ml-1 select-none text-lg">Artiheal</span>
           </div>
 
           <h2 className="font-bold text-lg my-7">Iniciar Sesión</h2>
           <form>
-
             <div className="grid w-full max-w items-center gap-2 mt-5">
               <Label htmlFor="email">Correo electronico</Label>
               <Input type="email" id="email" placeholder="Correo electronico" />
@@ -53,19 +54,24 @@ const Login = ({ handleLogin }: LoginProps) => {
               <Label htmlFor="sesion">Mantener iniciada mi sesión</Label>
             </div>
 
-            <Button className="my-4 py-3 px-6" variant="special" size="sp" onClick={handleLoginClick}>
+            <Button
+              className="my-4 py-3 px-6"
+              variant="special"
+              size="sp"
+              onClick={handleLoginClick}
+            >
               Iniciar Sesión
             </Button>
-           
+
             <span>
-                ¿No tienes cuenta?
-                <Link
-                  className="text-indigo-600 ml-1 hover:underline"
-                  to="/SignUp">
-                  Crear cuenta
-                </Link>
+              ¿No tienes cuenta?
+              <Link
+                className="text-indigo-600 ml-1 hover:underline"
+                to="/SignUp"
+              >
+                Crear cuenta
+              </Link>
             </span>
-           
           </form>
         </div>
       </main>
