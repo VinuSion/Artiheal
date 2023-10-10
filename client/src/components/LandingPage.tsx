a6ab08d73e454fb9683c14f300db1d7ce49bf3a7
Jospeeth <japertuzd@gmail.com>
2023-10-09 20:19:52 -0500

    changed landing page

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -45,8 +45,8 @@ const LandingPage = () => {
         <title>Welcome to Artiheal</title>
       </Helmet>
 
-      <div className="bg-[url('src/assets/bg-land1.jpg')] bg-cover bg-no-repeat bg-center min-h-screen">
-        <header className="bg-primary py-4 flex flex-row justify-between h-28 items-center p-2">
+      <div className="">
+        <header className="bg-primary py-4 flex flex-row justify-between h-28 items-center p-2 ">
           <div className="flex flex-row items-center text-white">
             <img className="h-20 w-20" src="/artiheal-logo.svg" alt="logo" />
             <div className="ml-4 flex flex-col">
@@ -54,6 +54,22 @@ const LandingPage = () => {
               <span className="text-lg">Cambiando Vidas</span>
             </div>
           </div>
+          <nav>
+            <ul className="flex flex-row gap-6">
+              <li className="text-white hover:scale-105 transform transition-transform duration-300">
+                <a href="#">Inicio</a>
+              </li>
+              <li className="text-white hover:scale-105 transform transition-transform duration-300">
+                <a href="#">Producto</a>
+              </li>
+              <li className="text-white hover:scale-105 transform transition-transform duration-300">
+                <a href="#">Servicios</a>
+              </li>
+              <li className="text-white hover:scale-105 transform transition-transform duration-300">
+                <a href="#">Sobre nosotros</a>
+              </li>
+            </ul>
+          </nav>
 
           <div className="flex flex-row space-x-4 mr-4">
             <Link to="/signup">
@@ -65,36 +81,43 @@ const LandingPage = () => {
           </div>
         </header>
 
-        <div className="flex mb-2">
-          <div className="w-1/2 m-24 p-3">
-            <h1 className="text-6xl font-bold leading-tight flex justify-center">
-              Lorem ipsum dolor sit amet.
-            </h1>
-            <p className="mt-4 mb-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et culpa
-              adipisci perferendis ut qui molestias perspiciatis a vel debitis,
-              sed ducimus quasi sint,soluta voluptates mollitia cupiditate
-              placeat? Consequuntur, quae?
-            </p>
-            <Button variant="landing">Más información</Button>
-          </div>
+        <main className="flex flex-col items-center justify-center  ">
+          <section className="flex my-10  place-items-center px-16">
+            <div className="w-1/2">
+              <h2 className="text-6xl font-bold leading-tight text-start">
+                Lorem ipsum dolor sit amet.
+              </h2>
+              <p className="my-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+                culpa adipisci perferendis ut qui molestias perspiciatis a vel
+                debitis, sed ducimus quasi sint, soluta voluptates mollitia
+                cupiditate placeat? Consequuntur, quae?
+              </p>
+              <Button variant="landing">Más información</Button>
+            </div>
 
-          <div className="w-1/2 mt-16 flex justify-center">
-            <img src="/src/assets/land11.png" className="w-80 h-80"/>
-          </div>
-        </div>
+            <div className="w-1/2 flex justify-center">
+              <img
+                src="/src/assets/land11.png"
+                className="h-80 w-80 object-contain"
+              />
+            </div>
+          </section>
 
-        <h2 className="text-4xl font-bold text-center">Nuestros servicios</h2>
+          <section className="my-16 bg-indigo-500 w-full">
+            <h2 className="text-4xl font-bold text-center mt-8">Servicios</h2>
 
-        <div className="flex justify-evenly">
-          {cardInfo.map((info, index) => (
-            <Card
-              key={index}
-              spanMessage={info.spanMessage}
-              buttonMessage={info.buttonMessage}
-            />
-          ))}
-        </div>
+            <div className="flex justify-evenly">
+              {cardInfo.map((info, index) => (
+                <Card
+                  key={index}
+                  spanMessage={info.spanMessage}
+                  buttonMessage={info.buttonMessage}
+                />
+              ))}
+            </div>
+          </section>
+        </main>
       </div>
     </div>
   );
