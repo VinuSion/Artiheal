df5da97aa304c576feb700e4cf70236226b1d76a
Jospeeth <japertuzd@gmail.com>
2023-10-28 21:51:24 -0500

    made dashboard section

--- client/src/components/auth-pages/Dashboard.tsx
+++ client/src/components/auth-pages/Dashboard.tsx
@@ -1,4 +1,8 @@
 import { Helmet } from "react-helmet-async";
+import ApexChart from "@/components/ui/distributedColumns";
+import CircleCalories from "@/components/ui/circleCalories";
+import CircleGoal from "@/components/ui/circleGoal";
+import CircleTime from "@/components/ui/circleTime";
 
 const Dashboard = () => {
   return (
@@ -6,9 +10,43 @@ const Dashboard = () => {
       <Helmet>
         <title>Dashboard | Artiheal</title>
       </Helmet>
-      <p>Dashboard Page</p>
-    </div>
-  )
-}
+      <h2 className="font-bold text-2xl sm:text-4xl p-4 text-primary">
+        Mi condicion fisica
+      </h2>
+      <main className="flex justify-center mb-6 h-full">
+        <article className="rounded-xl bg-background w-full sm:w-9/12 p-4 flex flex-col shadow-xl ">
+          <div className="flex flex-row gap-4 py-8 mb-8 justify-center flex-wrap border border-s rounded-md">
+            <div className="border border-s p-6 rounded-md ">
+              {" "}
+              <CircleCalories />
+            </div>
+            <div className="border border-s p-6 rounded-md ">
+              {" "}
+              <CircleTime />
+            </div>
+            <div className="border border-s p-6 rounded-md ">
+              {" "}
+              <CircleCalories />
+            </div>
+            <div className="border border-s p-6 rounded-md ">
+              {" "}
+              <CircleCalories />
+            </div>
+          </div>
 
-export default Dashboard
+          <div className=" py-6 flex flex-row justify-center gap-16 border border-s rounded-md flex-wrap">
+          <div className="border border-s p-6 rounded-md ">
+            <ApexChart />
+            </div>
+            
+            <div className="border border-s p-6 rounded-md ">
+            <CircleGoal />  
+            </div>
+          </div>
+        </article>
+      </main>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ client/src/components/ui/circleTime.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import Chart from "react-apexcharts";
+
+class CircleTime extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          series: [10],
+          options: {
+            chart: {
+              type: 'radialBar',
+              toolbar: {
+                show: true
+              }
+            },
+            plotOptions: {
+              radialBar: {
+                startAngle: -135,
+                endAngle: 225,
+                 hollow: {
+                  margin: 0,
+                  size: '70%',
+                  background: '#fff',
+                  image: undefined,
+                  imageOffsetX: 0,
+                  imageOffsetY: 0,
+                  position: 'front',
+                  dropShadow: {
+                    enabled: true,
+                    top: 3,
+                    left: 0,
+                    blur: 4,
+                    opacity: 0.24
+                  }
+                },
+                track: {
+                  background: '#fff',
+                  strokeWidth: '67%',
+                  margin: 0, // margin is in pixels
+                  dropShadow: {
+                    enabled: true,
+                    top: -3,
+                    left: 0,
+                    blur: 4,
+                    opacity: 0.35
+                  }
+                },
+            
+                dataLabels: {
+                  show: true,
+                  name: {
+                    offsetY: -10,
+                    show: true,
+                    color: '#888',
+                    fontSize: '17px'
+                  },
+                  value: {
+                    formatter: function(val) {
+                      return parseInt(val);
+                    },
+                    color: '#111',
+                    fontSize: '30px',
+                    show: true,
+                  }
+                }
+              }
+            },
+            fill: {
+              type: 'gradient',
+              gradient: {
+                shade: 'light',
+                type: 'horizontal',
+                shadeIntensity: 0.5,
+                gradientToColors: ['#745cff'],
+                opacityFrom: 1,
+                opacityTo: 1,
+                stops: [0, 100]
+              }
+            },
+            stroke: {
+              lineCap: 'round'
+            },
+            labels: ['Tiempo'],
+          },
+        
+        
+        };
+      }
+
+  render() {
+    return (
+      <Chart
+        options={this.state.options}
+        series={this.state.series}
+        height={200}
+        width={200}
+        type="radialBar"
+      />
+    );
+  }
+  
+}
+export default CircleTime;
+
