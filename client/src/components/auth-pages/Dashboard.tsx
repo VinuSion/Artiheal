3c20ad9049500b3d9b497354e0985c2209407e4c
William G (VinuSion) <revisionthegame@gmail.com>
2023-10-30 14:10:35 -0500

    ui updates

--- client/src/components/auth-pages/Dashboard.tsx
+++ client/src/components/auth-pages/Dashboard.tsx
@@ -1,10 +1,10 @@
 import { Helmet } from "react-helmet-async";
-import SplineArea from "@/components/ui/splineArea";
-import CircleCalories from "@/components/ui/circleCalories";
-import CircleGoal from "@/components/ui/circleGoal";
-import CircleTime from "@/components/ui/circleTime";
-import CircleSteps from "@/components/ui/circleSteps";
-import CircleDistance from "@/components/ui/circleDistance";
+import SplineArea from "@ui/splineArea";
+import CircleCalories from "@ui/circleCalories";
+import CircleGoal from "@ui/circleGoal";
+import CircleTime from "@ui/circleTime";
+import CircleSteps from "@ui/circleSteps";
+import CircleDistance from "@ui/circleDistance";
 
 const Dashboard = () => {
   const userInfoString = localStorage.getItem("userInfo")!;
@@ -15,14 +15,14 @@ const Dashboard = () => {
       <Helmet>
         <title>Dashboard | Artiheal</title>
       </Helmet>
-      <h2 className="font-bold text-2xl sm:text-4xl p-4 text-primary">
-        <span className="font-bold text-primary">
-          ¡Hola {userInfo.firstName}!
-        </span>{" "}
-        ¡Aquí está tu estado físico!
-      </h2>
       <main className="flex justify-center mb-6 h-full">
-        <article className="rounded-xl bg-background w-full sm:w-9/12 p-4 flex flex-col shadow-xl ">
+        <article className="rounded-xl bg-background w-full sm:w-9/12 p-4 flex flex-col shadow-xl">
+          <h2 className="font-bold text-2xl sm:text-4xl p-4 text-primary">
+            <span className="font-bold text-primary">
+              ¡Hola {userInfo.firstName}!
+            </span>{" "}
+            ¡Aquí está tu estado físico!
+          </h2>
           <div className="flex flex-row gap-4 py-8 mb-8 justify-center flex-wrap border border-s rounded-md">
             <div className="border border-s p-6 rounded-md ">
               {" "}
